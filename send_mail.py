fcb8045ddedbe8bb4713a8ab44fdce48fbf1417b
sherbakov <ashcherbakov@beeline.ru>
2014-10-29 19:06:10 +0300

    add send function

--- send_mail.py
+++ send_mail.py
@@ -1,19 +1,24 @@
 import smtplib
-
 from email.mime.text import MIMEText
 
 me = "admin@sherbakov.corbina.net"
 you = "ashcherbakov@beeline.ru"
 
-fp = open("text","r")
-msg = MIMEText(fp.read())
-fp.close()
+login = "user"
+password = "12345"
 
-msg['Subject'] = 'The contents of'  
-msg['From'] = me
-msg['To'] = you
 
-#print msg
-s = smtplib.SMTP('localhost')
-s.sendmail(me, [you], msg.as_string())
-s.quit()
+def send(login,password):
+	mesg = "login: %s\npass: %s\n" % (login, password)
+	msg = MIMEText(mesg)
+	
+	msg['Subject'] = 'Authentication data for FTTB devices'  
+	msg['From'] = me
+	msg['To'] = you
+
+	#print msg
+	s = smtplib.SMTP('localhost')
+	s.sendmail(me, [you], msg.as_string())
+	s.quit()
+
+send(login,password)
