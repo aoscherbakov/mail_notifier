1fbf278eb5fa68c1ab614cbda4730bdc0ad5392e
sherbakov <ashcherbakov@beeline.ru>
2014-10-31 16:04:48 +0300

    send from file

--- send_mail.py
+++ send_mail.py
@@ -1,29 +1,39 @@
+#!/usr/bin/python
 import smtplib
 from email.mime.text import MIMEText
 
-#me = "admin@sherbakov.corbina.net"
+me = "admin@sherbakov.corbina.net"
 #you = "ashcherbakov@beeline.ru"
 #
 #login = "user"
 #password = "12345"
 
-fp = open("users", "r")
 
-for user in fp:
-	print user
+with open("users","r") as f:
+	users = f.readlines()
 
+#remove whitespace/newline symbols
+for i in range(len(users)):
+	users[i] = users[i].rstrip()
 
-def send(login,password):
+def send(login,password,mail):
 	mesg = "login: %s\npass: %s\n" % (login, password)
 	msg = MIMEText(mesg)
 	
 	msg['Subject'] = 'Authentication data for FTTB devices'  
 	msg['From'] = me
-	msg['To'] = you
+	msg['To'] = mail
 
 	#print msg
+	print "sending message to %s" % mail
 	s = smtplib.SMTP('localhost')
-	s.sendmail(me, [you], msg.as_string())
+	s.sendmail(me, [mail], msg.as_string())
 	s.quit()
 
-#send(login,password)
+for user in users:
+	data = user.split(":")
+	mail = data[0]
+	password = data[1]
+	data = mail.split("@")
+	login = data[0]
+	send(login,password,mail)
